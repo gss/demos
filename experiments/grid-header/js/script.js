932042108a52ec522591bfc0f03c55ae0594591a
Nick <nick@narrowdesign.com>
2014-11-21 06:46:17 -0500

    mobile

--- experiments/grid-header/js/script.js
+++ experiments/grid-header/js/script.js
@@ -20,14 +20,14 @@ $(document).ready(function() {
   $(window).on('scroll',function(e){
     scrollHandler(e);
   })
-  $(window).bind('mousemove',function(e){
-    var x = e.pageX-(WIN_W/2);
-    var y = e.pageY-(WIN_H/2);
-    $('.head-cta').css({
-      '-webkit-transform-origin': '50% 50%',
-      '-webkit-transform': 'perspective(200px) rotateX('+y/-343+'deg) rotateY('+x/373+'deg)'
-    })
-  })
+  // $(window).bind('mousemove',function(e){
+  //   var x = e.pageX-(WIN_W/2);
+  //   var y = e.pageY-(WIN_H/2);
+  //   $('.head-cta').css({
+  //     '-webkit-transform-origin': '50% 50%',
+  //     '-webkit-transform': 'perspective(200px) rotateX('+y/-343+'deg) rotateY('+x/373+'deg)'
+  //   })
+  // })
  $(window).bind('touchstart',function(e){
     e.preventDefault(); 
     // isTouchDevice = true;
@@ -63,9 +63,12 @@ $(document).ready(function() {
   function pageResize (e) {
     WIN_H = WIN.height();
     WIN_W = WIN.width();
-    if(WIN_W > 1400){
+    if(WIN_W < 500){
       smallScreen = true;
     }else{
+      setTimeout(function(){
+        $('.head-cta').addClass('head-cta-target');
+      },3000)
     }
   }
   function scrollHandler(e){
