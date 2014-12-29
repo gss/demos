bc66628f90186086e25220053196d1157389ae0f
Trevor O' Brien <obrientrevor@yahoo.co.uk>
2014-12-29 17:04:58 +0000

    added filter buttons

--- experiments/flex-header/js/script.js
+++ experiments/flex-header/js/script.js
@@ -3,9 +3,20 @@ document.getElementById('with-sub-title').addEventListener('click',addSubTitle);
 
 function addSubTitle() {
 
-var element = document.getElementById("flex-head");
+  var element = document.getElementById("flex-head");
 
-element.classList.toggle("without-subtitle");
-element.classList.toggle("with-subtitle");
+  element.classList.toggle("without-subtitle");
+  element.classList.toggle("with-subtitle");
+
+}
+
+document.getElementById('with-cta').addEventListener('click',addCTA);
+
+function addCTA() {
+
+  var element = document.getElementById("flex-head");
+
+  element.classList.toggle("without-cta");
+  element.classList.toggle("with-cta");
 
 }
