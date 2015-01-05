314963cb635f0b84015af8e3bbea91361ebdebab
Nick <nick@narrowdesign.com>
2015-01-05 13:58:32 -0500

    flex CONFIGURATOR

--- experiments/flex-header/js/script.js
+++ experiments/flex-header/js/script.js
@@ -1,22 +1,39 @@
 
-document.getElementById('with-sub-title').addEventListener('click',addSubTitle);
+document.getElementById('sub-title-toggle').addEventListener('click',toggleSubTitle);
+document.getElementById('cta-toggle').addEventListener('click',toggleCTA);
+document.getElementById('bg-image-toggle').addEventListener('click',toggleBgImage);
+document.getElementById('body-text-toggle').addEventListener('click',toggleBodyText);
 
-function addSubTitle() {
+var element = document.getElementById("flex-head");
 
-  var element = document.getElementById("flex-head");
+function toggleSubTitle() {
 
   element.classList.toggle("without-subtitle");
   element.classList.toggle("with-subtitle");
+  this.classList.toggle("on");
 
 }
 
-document.getElementById('with-cta').addEventListener('click',addCTA);
-
-function addCTA() {
-
-  var element = document.getElementById("flex-head");
+function toggleCTA() {
 
   element.classList.toggle("without-cta");
   element.classList.toggle("with-cta");
+  this.classList.toggle("on");  
+
+}
+
+function toggleBgImage() {
+
+  element.classList.toggle("without-bg-image");
+  element.classList.toggle("with-bg-image");
+  this.classList.toggle("on");  
+
+}
+
+function toggleBodyText() {
+
+  element.classList.toggle("without-body-text");
+  element.classList.toggle("with-body-text");
+  this.classList.toggle("on");  
 
 }
